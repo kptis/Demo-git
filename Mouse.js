544a5d5ebb5376588720405f9ab6ca5942753c96
Khong <shinkhong97@gmail.com>
2021-02-24 13:40:26 -0800

    Add lai Mouse.js

--- Mouse.js
+++ Mouse.js
@@ -1,3 +1,15 @@
-function Mouse(){
-	
+function Mouse(name){
+	//Thay doi lan 1 
+	//Van la lan thay doi 1
+	this.name= name;
+	this.dead = false;
+
 }
+Mouse.prototype.die = function(){
+	this.dead = true;
+};
+//Xem git diff  sau khi them dong nay 
+//ca dong nay nua 
+
+module.exorts = Mouse;
+
